bug fixes ( minus sign missing and units error)

1d-TDHF/src/densities.py
@@ -20,7 +20,7 @@ def rho(psi_array):
     '''
     rho_p = np.zeros(len(grid))
     rho_n = np.zeros(len(grid))
-    for n in range(nmax):
+    for n in range(nmax+1):
         for l in range(lmax+1):
             for s in range(len(spin)):
                 j = l + spin[s]

1d-TDHF/src/fields.py
@@ -40,9 +40,9 @@ def yukArr(rhoArr):
         I = 0.0 
         for j in range(i):
             rp = grid[j]
-            I += rhoArr[j]*(np.exp(-abs(r-rp)/a)/abs(r-rp))*step_size
+            I += rhoArr[j]*(np.exp(-abs(r-rp)/a)/abs(r-rp))*step_size*rp**2
         Vyuk[i] = I
-    return Vyuk*aV0
+    return Vyuk*aV0*4*np.pi
 
 @jit(nopython=True,parallel=True)
 def yukArr2(rhoArr):
@@ -56,7 +56,7 @@ def yukArr2(rhoArr):
                 denom = np.sqrt(r**2 + rp**2 - 2*r*rp*np.cos(thetap))
                 I += rhoArr[j]*(num/denom)*step_size
         Vyuk[i] = I
-    return Vyuk*aV0
+    return Vyuk*aV0*2*np.pi
 @jit(nopython=True,parallel=True)
 def coulombArr(rhoArr):
     Vc12 = np.zeros(len(grid))
@@ -70,8 +70,8 @@ def coulombArr(rhoArr):
     for i in range(0,len(grid)):
         Vcinf += rhoArr[i]*grid[i]*step_size
     
-    Vc = 4*np.pi*e2*(Vc12 + Vcinf)
-    return Vc*4*np.pi*e2
+    Vc = 4*np.pi*e2*(Vc12 + Vcinf) #- e2*(3.0/np.pi)**(1.0/3.0)*rhoArr**(1.0/3.0)
+    return Vc
 
 def centriforceArr(l):
     result = l*(l+1)/grid**2

1d-TDHF/src/main.py
@@ -43,7 +43,7 @@ plt.legend()
 plt.show()
 ## Construct initial g_array. Every wavefunction will have one of these functions
 ## associated with it. 
-fArr = np.zeros((2,nmax,lmax+1,len(spin),len(grid)))
+fArr = np.zeros((2,nmax+1,lmax+1,len(spin),len(grid)))
 ### 
 E_array = []
 D = solvers.getNumerov_matrix()
@@ -55,27 +55,28 @@ for i in range(1,len(grid)-1):
 print(f'A = {A}')
 for nter in range(0,nIter):
     for q in range(0,2):
-        for n in range(nmax):
+        for n in range(nmax+1):
             for l in range(lmax+1):
                 for s in range(len(spin)):
                     j = l + spin[s]
+                    #print(q,n,l,s,j)
                     ## fill gArr with initial values for each nucleus. We are using units where m_p = m_n = 1.
                     if q == 0:
-                        fArr[q][n][l][s] = (hArr + V_c) - fields.centriforceArr(l)*hb2m0 #
+                        fArr[q][n][l][s] = (hArr + V_c) + fields.centriforceArr(l)*hb2m0#
                     else: 
-                        fArr[q][n][l][s] = hArr - fields.centriforceArr(l)*hb2m0
+                        fArr[q][n][l][s] = hArr + fields.centriforceArr(l)*hb2m0
                     #plt.plot(grid,fArr[q][n][l][s],label='f function')
                     #plt.legend()
                     #plt.show()
                     V_matrix = np.diag(fArr[q][n][l][s])
-                    H = D + V_matrix
+                    H = D - V_matrix/hb2m0
                     #E_0,psi_array[q][n][l][s] = solvers.solve_Numerov(psi_array[q][n][l][s],E,dE,fArr[q][n][l][s])
                     energies[q][n][l][s],psi_array[q][n][l][s] = solvers.MatrixNumerovSolve(H)
-                    energies[q][n][l][s] = -1*energies[q][n][l][s]*hb2m0
+                    energies[q][n][l][s] = energies[q][n][l][s]
                     
                     
                     
-                    print(energies[q][n][l][s][0])
+                    print(-1*energies[q][n][l][s][0]*hb2m0)
                     #plt.plot(grid[:600],psi_array[q][n][l][s][:600])
                     #plt.xlabel('r')
                     #plt.ylabel(r'$\psi(r)$')
@@ -112,14 +113,14 @@ for nter in range(0,nIter):
     ### recalculate hamiltonian with new densities
     hArr = functionals.h_BKN(rhoArr[0])         
 
-    #plt.plot(grid,V_c)
-    #plt.xlabel('r')
-    #plt.ylabel(r'$Vc$')
-    #plt.title(f'{nter}')
-    #plt.show()    
+    plt.plot(grid,V_c)
+    plt.xlabel('r')
+    plt.ylabel(r'$Vc$')
+    plt.title(f'{nter}')
+    plt.show()    
     
-    #plt.plot(grid,hArr)
-    #plt.xlabel('r')
-    #plt.ylabel(r'$h$')
-    #plt.title(f'{nter}')
-    #plt.show() 
+    plt.plot(grid,hArr)
+    plt.xlabel('r')
+    plt.ylabel(r'$h$')
+    plt.title(f'{nter}')
+    plt.show() 

1d-TDHF/src/wf.py
@@ -4,7 +4,6 @@ from scipy.special import assoc_laguerre
 from init import *
 
 
-
 '''
 NOTE: HYDROGEN WAVEFUNCTIONS NOT WORKING
 '''
@@ -73,28 +72,27 @@ def initWfs(N,Z,name='HO'):
     if name == 'HO':
         psi_array = np.zeros((2,nmax+1,lmax+1,len(spin),len(grid)))
         energies_array = np.zeros((2,nmax+1,lmax+1,len(spin),1))
+        l_lim = 0
         for q in range(2):
             for n in range(nmax+1):
                 for l in range(lmax+1):
                     for s in range(len(spin)):
-                        if n == 0:
-                            l = 0
                         print(q,n,l,s)
-                        psi_func = get_wfHO_radial(n, l)#wf.initWfs(name='hydrogen',n=0,l=0)
+                        psi_func = get_wfHO_radial(n,l) 
                         eval_psi = psi_func(grid)
-                        psi_array[q,n,l,s] = eval_psi/np.linalg.norm(eval_psi)
+                        psi_array[q,n,l,0] = eval_psi/np.linalg.norm(eval_psi)
         return psi_array,energies_array
     elif name == 'hydrogen':
-        psi_array = np.zeros((2,nmax,lmax+1,len(spin),len(grid)))
-        energies_array = np.zeros((2,nmax,lmax+1,len(spin),1))
+        psi_array = np.zeros((2,nmax+1,lmax+1,len(spin),len(grid)))
+        energies_array = np.zeros((2,nmax+1,lmax+1,len(spin),1))
         for q in range(2):
-            for n in range(nmax):
+            for n in range(nmax+1):
                 for l in range(lmax+1):
                     if n == 0:
                         l = 0
                     for s in range(len(spin)):
                         #print(q,n+1,l,s)
-                        psi_func = get_WfHydrogen_radial(1, 0)#wf.initWfs(name='hydrogen',n=0,l=0)
+                        psi_func = get_wfHO_radial(n=n+q,l=s+l)
                         eval_psi = psi_func(grid)
                         psi_array[q,n,l,s] = eval_psi/np.linalg.norm(eval_psi)
         return psi_array,energies_array