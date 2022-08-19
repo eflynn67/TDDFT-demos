b4fe2cff0bc2fbd48a7d988227f613d7762bb97e
Eric Flynn <flynner6@msu.edu>
2022-08-19 17:45:58 -0400

    update. no bug found yet

--- GP-demo/src/matrix.py
+++ GP-demo/src/matrix.py
@@ -31,9 +31,40 @@ def get_H_func(name='HO'):
             off_diag = np.zeros(dim)
             off_diag[1] = 1
             H = -1*(-2*np.identity(dim) + sci.linalg.toeplitz(off_diag))/(step_size**2) + np.diag(potentials.V_HO(grid,alpha)) \
-                + np.diag(potentials.V_rho(psi,psiStar,q))
+                +np.diag(potentials.V_rho(psi,psiStar,q))
             return H
         return HO
+    elif name=='HO_PT':
+        def HO_PT(psi,psiStar,mass,alpha,q):
+            '''
+            Uses 2nd order finite difference scheme to construct a discretized differential 
+            H operator for the GP potential.
+        
+            Parameters
+            ----------
+            psi : ndArray
+                wavefunction.
+            psiStar : ndArray
+                wavefunction conjugate dual.
+            mass : TYPE
+                DESCRIPTION.
+            alpha : TYPE
+                DESCRIPTION.
+            q : TYPE
+                DESCRIPTION.
+        
+            Returns
+            -------
+            H : TYPE
+                DESCRIPTION.
+        
+            '''
+            dim = len(grid)
+            off_diag = np.zeros(dim)
+            off_diag[1] = 1
+            H = -1*(-2*np.identity(dim,dtype=complex) + np.array(sci.linalg.toeplitz(off_diag),dtype=complex))/(step_size**2) + np.diag(potentials.V_HO(grid,alpha)*(1.0j*potentials.V_HO(grid,alpha))**q)
+            return H
+        return HO_PT
     elif name=='quartic':
         def quartic(psi,psiStar,mass,alpha,q):
             '''
