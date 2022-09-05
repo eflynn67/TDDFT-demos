29993238a10970f6ee0b4e0664a6009b54c35856
Eric Flynn <flynner6@msu.edu>
2022-09-05 11:41:14 -0400

    sync update

--- spectral-demo/example_2_periodic.py
+++ spectral-demo/example_2_periodic.py
@@ -3,28 +3,21 @@ import matplotlib.pyplot as plt
 from scipy.special import eval_chebyt
 '''
 Here we use Fourier basis to solve a periodic boundary condition problem.
-
-1) y'' + x y' = x 
-with Dirichelt BCs
-
-Next, we tackle a non-linear equation: Painleve I
-
-y'' = 6y^2 + x
-with Dirichlet BCS y(0) = 0 and y(1) = 0. This is a different beast. We use the self consistent approach
-to solving this equation. We start our initial guess using x_{j}(x_{j}-1)
+First we solve a very simple one
+1) y'' = - y
+with Periodic boundary conditions y(0) = y(2 pi) = 0
 '''
 
-def getGaussLobatto(N,interval=(-1,1)):
+def getCollocation(N):
     '''
     Gets the Gauss-Lobatto points of the Mth chebyshev polynomials.
     Formula taken from Spectral Methods in Fluid Dynamics (1988)
     N: integer
     max number of basis elements
     '''
-    GSPnts = np.zeros(N+1)
-    for l in range(N+1):
-        GSPnts[l] = .5*(interval[0] + interval[1]) - .5*(interval[1] -interval[0]) \
-            *np.cos(l*np.pi/N)
+    CPnts = np.zeros(N+1)
+    for j in range(N+1):
+        CPnts[i] = 2*j*np.pi/(N+1)
     return GSPnts
 
 def c_coeff(l,N):
@@ -54,18 +47,12 @@ def getDerMatrix(CPnts,BC = 'dirichlet'):
 
     '''
     N = len(CPnts) - 1 
-    D = np.zeros((N+1,N+1))
+    S = np.zeros((N+1,N+1))
     for i in range(N+1):
         for j in range(N+1):
-            if i == 0 and j == 0:
-                D[i][j] = -(2 * N**2 + 1)/6
-            elif i == N  and j == N:
-                D[i][j] = (2 * N**2 + 1)/6
-            elif i == j and j <= N and j >= 1 and i <= N  and i >= 1 :
-                D[i][j] = - CPnts[j]/(2*(1 - CPnts[j]**2))
-            else:
-                D[i][j] = c_coeff(i,N)*(-1)**(i+j) /(c_coeff(j,N)*(CPnts[i] - CPnts[j])) 
-    return D
+            if i != j:
+                S[i][j] = .5*(-1)**(i+j) * np.cot((i-j)*np.pi/N)
+    return S
 
 N = 100 # number of basis functions .
 
